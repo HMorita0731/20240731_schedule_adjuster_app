c91280dbda53d420fb686a23fd2a0cd37d870a80
HMorita0731 <alphy.versu@gmail.com>
2024-08-09 13:39:03 +0900

    20240809hm

--- lib/schedule_adj_app_web/live/event_live/input.ex
+++ lib/schedule_adj_app_web/live/event_live/input.ex
@@ -14,10 +14,10 @@ defmodule ScheduleAdjAppWeb.EventLive.Input do
 
   def render(assigns) do
     ~H"""
-    プルテスト
     <h2 class="text-4xl"><%= @event_detail.title %></h2>
      <% date_list = make_date_list(@event_dates)%>
      <% datetime_list = make_datetime_list(@event_dates)%>
+     <%= %>
      <%= if length(date_list) > 0 do %>
   <!--@はsocket.assignsのキー名そのまま描く-->
   <div :for={date <- date_list} class="my-4 w-full">
@@ -248,7 +248,19 @@ defmodule ScheduleAdjAppWeb.EventLive.Input do
 
     def handle_event("insert_user_dates", %{"user_input" => params}, socket) do
       IO.inspect(params)
-      {:noreply, socket}
+      user_datetime_id_list = make_user_datetime_event_date_id(socket.assigns.event_dates,socket.assigns.add_user_datetime)
+
+      socket =
+      case Users.insert_user_data(user_datetime_id_list,params,socket.assigns.event_detail.id) do
+        {:ok,_transaction} ->
+          socket
+          |> put_flash(:info, "日程を登録しました")
+          |> redirect(to: ~p"/event/show/#{socket.assigns.event_detail.id}")
+        {:error, action, _, _} ->
+          socket
+          |> put_flash(:error, "#{action}で登録に失敗しました")
+        end
+        {:noreply, socket}
     end
 
   def make_date_list(event_date_list) do
@@ -266,6 +278,13 @@ defmodule ScheduleAdjAppWeb.EventLive.Input do
     |> Enum.map(fn [str_date, str_time] -> [str_date,String.slice(str_time,0,5)] end)
   end
 
+  def make_user_datetime_event_date_id(event_date_list,user_datetime) do
+    make_datetime_list(event_date_list)
+    |>Enum.zip(Enum.map(event_date_list,fn %{id: id} -> id end))
+    |>Enum.filter(fn {event_date,_id} -> event_date in user_datetime end)
+    |>Enum.map(fn{_event_date,id} -> id end)
+  end
+
 
   # ソケットにcsをフォーム型にして挿入
   defp assign_form(socket, cs) do
@@ -278,4 +297,17 @@ defmodule ScheduleAdjAppWeb.EventLive.Input do
   # time = DateTime.to_string(datetime)|>String.split(" ") |> Enum.at(1)
   # [date, time]
   # end #convert_from_datetime
+
+# [error] GenServer #PID<0.966.0> terminating
+# ** (FunctionClauseError) no function clause matching in ScheduleAdjApp.Users.insert_user_data/3
+#     (schedule_adj_app 0.1.0) lib/schedule_adj_app/users.ex:22:
+#     ScheduleAdjApp.Users.insert_user_data([20, 21, 22, 23, 24, 25, 26, 27], %{"memo" => "テスト入力", "name" => "仮ユーザー", "pass" => "pass"}, 4)
+#     (schedule_adj_app 0.1.0) lib/schedule_adj_app_web/live/event_live/input.ex:255: ScheduleAdjAppWeb.EventLive.Input.handle_event/3
+#     (phoenix_live_view 1.0.0-rc.6) lib/phoenix_live_view/channel.ex:508: anonymous fn/3 in Phoenix.LiveView.Channel.view_handle_event/3
+#     (telemetry 1.2.1) c:/Users/artis/Desktop/elixir_2024_DE/schedule_adj_app/deps/telemetry/src/telemetry.erl:321: :telemetry.span/3
+#     (phoenix_live_view 1.0.0-rc.6) lib/phoenix_live_view/channel.ex:260: Phoenix.LiveView.Channel.handle_info/2
+#     (stdlib 5.0) gen_server.erl:1077: :gen_server.try_handle_info/3
+#     (stdlib 5.0) gen_server.erl:1165: :gen_server.handle_msg/6
+#     (stdlib 5.0) proc_lib.erl:251: :proc_lib.wake_up/3
+
 end
