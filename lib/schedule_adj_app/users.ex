080bb5af553ad81707b2e47b6e72aaaaa371b7bf
HMorita0731 <alphy.versu@gmail.com>
2024-08-09 09:54:21 +0900

    fixed

--- lib/schedule_adj_app/users.ex
+++ lib/schedule_adj_app/users.ex
@@ -21,6 +21,29 @@ defmodule ScheduleAdjApp.Users do
   end
 
   # change_user_for_event_insert
+  def insert_user_data(params,user_datatime_list) do
+  Enum.zip(1..length(user_datetime_list), user_datetime_list)
+    |> Enum.reduce(
+      # この行から第二引数
+      Multi.new()
+      |> Multi.insert(:user, fn %{event: event} ->
+        User.changeset(%User{event_id: event.id}, params)
+      end),
+      # この行から第三引数,multiに処理し終えたマルチ構造体が入っていく
+      fn {index, datetime}, multi ->
+        Multi.insert(
+          multi,
+          "user_date_#{index}",# ←操作の名前
+          fn %{event: event} ->
+            EventDate.changeset(%EventDate{event_id: event.id}, %{event_dates: datetime})
+          end
+        )
+      end
+    )
+
+    # reduce
+    |> Repo.transaction()
+  end
 end
 
 # mod
